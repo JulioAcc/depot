c4e53f1a57aeec881eebdcd882bb2ba5c3adb207
Julio Castillo <julioandrescc@gmail.com>
2014-05-03 01:24:55 -0600

    Section C

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ActiveRecord::Base
   
+  default_scope { order('title') }  
+
   validates :title, :description, :image_url, :presence => true
   
   validates :price, :numericality => {:greather_than_or_equal_to => 0.01}
